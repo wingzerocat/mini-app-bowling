8192c01ca490080964864540dec0ed10bd559c98
wingzerocat <mikechenlou@gmail.com>
2019-03-25 11:47:04 -0500

    Shows final score

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -71,107 +71,112 @@ class App extends React.Component {
         var newBonus = this.state.bonus;
         var total = this.state.total;
 
-        if (Math.floor(activeFrame) > 2) {
-            if (newBonus[Math.floor(activeFrame) - 3][0] === 2) {
-                newBonus[Math.floor(activeFrame) - 3][1] += points;
-                newBonus[Math.floor(activeFrame) - 3][0] = 1;
 
+        if (activeFrame < 11) {
+            if (Math.floor(activeFrame) > 2) {
+                if (newBonus[Math.floor(activeFrame) - 3][0] === 2) {
+                    newBonus[Math.floor(activeFrame) - 3][1] += points;
+                    newBonus[Math.floor(activeFrame) - 3][0] = 1;
+    
+                    this.setState({
+                        total: total,
+                        bonus: newBonus
+                    })
+                } else if (newBonus[Math.floor(activeFrame) - 3][0] === 1) {
+                    newBonus[Math.floor(activeFrame) - 3][1] += points;
+                    newBonus[Math.floor(activeFrame) - 3][0] = 0;
+                    // total += points * 2;
+    
+                    var prevFrame = this.state['frame_' + Math.floor(activeFrame - 2)];
+                    
+                    if (Math.floor(activeFrame) > 3) {
+                        var oldFrame = this.state['frame_' + Math.floor(activeFrame - 3)];
+                    } else {
+                        var oldFrame = [null, null, 0];
+                    }
+    
+                    prevFrame[2] = prevFrame[2] + newBonus[Math.floor(activeFrame) - 3][1] + oldFrame[2];
+                    total = prevFrame[2];
+                    this.setState({
+                        ['frame_' + (Math.floor(activeFrame) -2)]: prevFrame,
+                        bonus: newBonus,
+                        total: total
+                    })
+                }
+            }
+    
+            if (Math.floor(activeFrame) > 1) {
+                console.log('check')
+                if (newBonus[Math.floor(activeFrame) - 2][0] === 2) {
+                    newBonus[Math.floor(activeFrame) - 2][1] += points;
+                    newBonus[Math.floor(activeFrame) - 2][0] = 1;
+    
+                    this.setState({
+                        total: total,
+                        bonus: newBonus
+                    })
+                } else if (newBonus[Math.floor(activeFrame) - 2][0] === 1) {
+                    newBonus[Math.floor(activeFrame) - 2][1] += points;
+                    newBonus[Math.floor(activeFrame) - 2][0] = 0;
+                    var prevFrame = this.state['frame_' + Math.floor(activeFrame - 1)];
+                    
+                    if (Math.floor(activeFrame) > 2) {
+                        var oldFrame = this.state['frame_' + Math.floor(activeFrame - 2)];
+                    }  else {
+                        var oldFrame = [null, null, 0];
+                    }
+    
+                    prevFrame[2] = prevFrame[2] + newBonus[Math.floor(activeFrame) - 2][1] + oldFrame[2];
+                    total = prevFrame[2];
+    
+                    this.setState({
+                        ['frame_' + (Math.floor(activeFrame) -1)]: prevFrame,
+                        bonus: newBonus,
+                        total: total
+                    })
+                }
+            }
+    
+            if (points === 10) {
+                frame[1] = 'X';
+                frame[2] = 10;
+                newBonus[activeFrame - 1][0] = 2;
                 this.setState({
-                    total: total,
+                    ['frame_' + activeFrame]: frame,
+                    activeFrame: activeFrame + 1,
+                    bonus: newBonus,
+                    total: total + 10
+                })
+            } 
+            else if (frame[0] + points === 10) {
+                frame[1] = '/';
+                frame[2] = 10;
+                newBonus[Math.floor(activeFrame) - 1][0] = 1;
+                this.setState({
+                    ['frame_' + Math.floor(activeFrame)]: frame,
+                    activeFrame: activeFrame + 0.5,
                     bonus: newBonus
                 })
-            } else if (newBonus[Math.floor(activeFrame) - 3][0] === 1) {
-                newBonus[Math.floor(activeFrame) - 3][1] += points;
-                newBonus[Math.floor(activeFrame) - 3][0] = 0;
-                // total += points * 2;
-
-                var prevFrame = this.state['frame_' + Math.floor(activeFrame - 2)];
-                
-                if (Math.floor(activeFrame) > 3) {
-                    var oldFrame = this.state['frame_' + Math.floor(activeFrame - 3)];
-                } else {
-                    var oldFrame = [null, null, 0];
-                }
-
-                prevFrame[2] = prevFrame[2] + newBonus[Math.floor(activeFrame) - 3][1] + oldFrame[2];
-                total = prevFrame[2];
+            } else if (activeFrame % 1 === 0) {
+                console.log('first');
+                frame[0] = points;
+                frame[2] = points + total;
                 this.setState({
-                    ['frame_' + (Math.floor(activeFrame) -2)]: prevFrame,
-                    bonus: newBonus,
-                    total: total
+                    ['frame_' + Math.floor(activeFrame)]: frame,
+                    activeFrame: activeFrame + 0.5,
+                    total: total + points
+                })
+            } else if (activeFrame % 1 !== 0) {
+                frame[1] = points;
+                frame[2] = total + points;  
+                this.setState({
+                    ['frame_' + Math.floor(activeFrame)]: frame,
+                    activeFrame: activeFrame + 0.5,
+                    total: total + points
                 })
             }
-        }
+        } else {
 
-        if (Math.floor(activeFrame) > 1) {
-            console.log('check')
-            if (newBonus[Math.floor(activeFrame) - 2][0] === 2) {
-                newBonus[Math.floor(activeFrame) - 2][1] += points;
-                newBonus[Math.floor(activeFrame) - 2][0] = 1;
-
-                this.setState({
-                    total: total,
-                    bonus: newBonus
-                })
-            } else if (newBonus[Math.floor(activeFrame) - 2][0] === 1) {
-                newBonus[Math.floor(activeFrame) - 2][1] += points;
-                newBonus[Math.floor(activeFrame) - 2][0] = 0;
-                var prevFrame = this.state['frame_' + Math.floor(activeFrame - 1)];
-                
-                if (Math.floor(activeFrame) > 2) {
-                    var oldFrame = this.state['frame_' + Math.floor(activeFrame - 2)];
-                }  else {
-                    var oldFrame = [null, null, 0];
-                }
-
-                prevFrame[2] = prevFrame[2] + newBonus[Math.floor(activeFrame) - 2][1] + oldFrame[2];
-                total = prevFrame[2];
-
-                this.setState({
-                    ['frame_' + (Math.floor(activeFrame) -1)]: prevFrame,
-                    bonus: newBonus,
-                    total: total
-                })
-            }
-        }
-
-        if (points === 10) {
-            frame[1] = 'X';
-            frame[2] = 10;
-            newBonus[activeFrame - 1][0] = 2;
-            this.setState({
-                ['frame_' + activeFrame]: frame,
-                activeFrame: activeFrame + 1,
-                bonus: newBonus,
-                total: total + 10
-            })
-        } 
-        else if (frame[0] + points === 10) {
-            frame[1] = '/';
-            frame[2] = 10;
-            newBonus[Math.floor(activeFrame) - 1][0] = 1;
-            this.setState({
-                ['frame_' + Math.floor(activeFrame)]: frame,
-                activeFrame: activeFrame + 0.5,
-                bonus: newBonus
-            })
-        } else if (activeFrame % 1 === 0) {
-            console.log('first');
-            frame[0] = points;
-            frame[2] = points + total;
-            this.setState({
-                ['frame_' + Math.floor(activeFrame)]: frame,
-                activeFrame: activeFrame + 0.5,
-                total: total + points
-            })
-        } else if (activeFrame % 1 !== 0) {
-            frame[1] = points;
-            frame[2] = total + points;  
-            this.setState({
-                ['frame_' + Math.floor(activeFrame)]: frame,
-                activeFrame: activeFrame + 0.5,
-                total: total + points
-            })
         }
     }
 
@@ -200,6 +205,17 @@ class App extends React.Component {
                 <button onClick={() => this.newGame()}>Restart</button>
                 <Buttons score={this.score.bind(this)}/>
                 <Frames list={frames} frames={this.state}/>
+
+                    {this.state.activeFrame >= 11 ? 
+                    <div>
+                        <div>
+                            {`You bowled ${this.state.total}`}
+                        </div>
+                        <button onClick={() => this.newGame()}>
+                            New Game?
+                        </button>
+                    </div>
+                    : ''}
             </div>
         )
     }
